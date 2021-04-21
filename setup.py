c81986ebc7cb419ea694ee80a32264bbecb7ef70
0xBSC-Project <82927041+0xBSC-Project@users.noreply.github.com>
2021-04-20 22:30:26 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="bscscan-python",
+    version="1.0.0",
+    description="A python API for bscscan.com.",
+    url="https://github.com/pcko1/bscscan-python",
+    author="Panagiotis-Christos Kotsias",
+    author_email="kotsias.pan@gmail.com",
+    license="MIT",
+    packages=[
+        "bscscan",
+        "bscscan.configs",
+        "bscscan.enums",
+        "bscscan.modules",
+        "bscscan.utils",
+    ],
+    install_requires=["requests"],
+    include_package_data=True,
+    zip_safe=False,
+)
